108656cf0c7b83eb1f8b0e0b320da82d58785d9e
suhasarali <149100577+suhasarali@users.noreply.github.com>
2024-10-04 14:17:09 +0530

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,10 @@
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        greet();
+    }
+
+    public static void greet() {
+        System.out.println("Greetings from Git!");
+    }
+}
